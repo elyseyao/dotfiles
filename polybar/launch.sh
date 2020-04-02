59ad9a198f1db706a7e568a2840f64dc811b25de
Elyse Yao <elyseyao@gmail.com>
2020-04-02 11:44:16 -0700

    modified polybar names, added maim/volume to i3 config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,8 +5,7 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example >>/tmp/polybar1.log 2>&1 &
+polybar main >>/tmp/polybar1.log 2>&1 &
 
 echo "Bars launched..."
